d770318d4df96a5b4a7227305e7d3c0151e1998d
Herao <Herao@atguigu.com>
2023-02-26 15:17:53 +0800

    first

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,10 @@
+/**
+ * @Auther:我家二狗子
+ * @Date:2023/2/26 15:16)
+ * Description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
